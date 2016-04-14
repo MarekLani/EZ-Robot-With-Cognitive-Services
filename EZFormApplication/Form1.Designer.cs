3fc239490fb6d8a7cf49ca585682c6bff5363e48
ptp <pedro.pereira@intelidata.pt>
2016-04-14 12:32:19 -0400

    add show video (picture frames)

--- EZFormApplication/Form1.Designer.cs
+++ EZFormApplication/Form1.Designer.cs
@@ -28,14 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.IpAddressTB = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.TakeAPicButton = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.DebugTextBox = new System.Windows.Forms.TextBox();
-            this.ImageFileNameTB = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.ImageFileNameTB = new System.Windows.Forms.TextBox();
+            this.StartCameraButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.FpsLabel = new System.Windows.Forms.Label();
+            this.fpsLabelLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.DebugTextBox = new System.Windows.Forms.TextBox();
+            this.OneSecondTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -55,64 +66,140 @@
             this.IpAddressTB.TabIndex = 1;
             this.IpAddressTB.Text = "192.168.18.85";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TakeAPicButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ImageFileNameTB);
+            this.groupBox1.Location = new System.Drawing.Point(15, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(411, 87);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Picture";
+            // 
             // TakeAPicButton
             // 
-            this.TakeAPicButton.Location = new System.Drawing.Point(191, 13);
+            this.TakeAPicButton.Location = new System.Drawing.Point(71, 45);
             this.TakeAPicButton.Name = "TakeAPicButton";
             this.TakeAPicButton.Size = new System.Drawing.Size(75, 23);
-            this.TakeAPicButton.TabIndex = 2;
+            this.TakeAPicButton.TabIndex = 9;
             this.TakeAPicButton.Text = "Take a PIC";
             this.TakeAPicButton.UseVisualStyleBackColor = true;
             this.TakeAPicButton.Click += new System.EventHandler(this.TakeAPicButton_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(15, 98);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(160, 120);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
-            // DebugTextBox
-            // 
-            this.DebugTextBox.Location = new System.Drawing.Point(15, 224);
-            this.DebugTextBox.Multiline = true;
-            this.DebugTextBox.Name = "DebugTextBox";
-            this.DebugTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.DebugTextBox.Size = new System.Drawing.Size(414, 98);
-            this.DebugTextBox.TabIndex = 4;
-            // 
-            // ImageFileNameTB
-            // 
-            this.ImageFileNameTB.Location = new System.Drawing.Point(73, 54);
-            this.ImageFileNameTB.Name = "ImageFileNameTB";
-            this.ImageFileNameTB.Size = new System.Drawing.Size(356, 20);
-            this.ImageFileNameTB.TabIndex = 5;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Location = new System.Drawing.Point(10, 19);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 13);
-            this.label2.TabIndex = 6;
+            this.label2.TabIndex = 8;
             this.label2.Text = "Image File";
+            // 
+            // ImageFileNameTB
+            // 
+            this.ImageFileNameTB.Location = new System.Drawing.Point(71, 19);
+            this.ImageFileNameTB.Name = "ImageFileNameTB";
+            this.ImageFileNameTB.Size = new System.Drawing.Size(321, 20);
+            this.ImageFileNameTB.TabIndex = 7;
+            // 
+            // StartCameraButton
+            // 
+            this.StartCameraButton.Location = new System.Drawing.Point(144, 15);
+            this.StartCameraButton.Name = "StartCameraButton";
+            this.StartCameraButton.Size = new System.Drawing.Size(105, 23);
+            this.StartCameraButton.TabIndex = 10;
+            this.StartCameraButton.Tag = "Start Camera|Stop Camera";
+            this.StartCameraButton.Text = "Start Camera";
+            this.StartCameraButton.UseVisualStyleBackColor = true;
+            this.StartCameraButton.Click += new System.EventHandler(this.StartCameraButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.FpsLabel);
+            this.groupBox2.Controls.Add(this.fpsLabelLabel);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(15, 135);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(411, 154);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Video";
+            // 
+            // FpsLabel
+            // 
+            this.FpsLabel.AutoSize = true;
+            this.FpsLabel.Location = new System.Drawing.Point(294, 43);
+            this.FpsLabel.Name = "FpsLabel";
+            this.FpsLabel.Size = new System.Drawing.Size(0, 13);
+            this.FpsLabel.TabIndex = 11;
+            this.FpsLabel.Visible = false;
+            // 
+            // fpsLabelLabel
+            // 
+            this.fpsLabelLabel.AutoSize = true;
+            this.fpsLabelLabel.Location = new System.Drawing.Point(191, 43);
+            this.fpsLabelLabel.Name = "fpsLabelLabel";
+            this.fpsLabelLabel.Size = new System.Drawing.Size(97, 13);
+            this.fpsLabelLabel.TabIndex = 9;
+            this.fpsLabelLabel.Text = "frames per second:";
+            this.fpsLabelLabel.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.DebugTextBox);
+            this.groupBox3.Location = new System.Drawing.Point(15, 296);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(411, 190);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Info";
+            // 
+            // DebugTextBox
+            // 
+            this.DebugTextBox.Location = new System.Drawing.Point(11, 19);
+            this.DebugTextBox.Multiline = true;
+            this.DebugTextBox.Name = "DebugTextBox";
+            this.DebugTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.DebugTextBox.Size = new System.Drawing.Size(381, 160);
+            this.DebugTextBox.TabIndex = 5;
+            // 
+            // OneSecondTimer
+            // 
+            this.OneSecondTimer.Interval = 1000;
+            this.OneSecondTimer.Tick += new System.EventHandler(this.OneSecondTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 354);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.ImageFileNameTB);
-            this.Controls.Add(this.DebugTextBox);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.TakeAPicButton);
+            this.ClientSize = new System.Drawing.Size(444, 495);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.StartCameraButton);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.IpAddressTB);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,11 +209,18 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox IpAddressTB;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button TakeAPicButton;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox DebugTextBox;
-        private System.Windows.Forms.TextBox ImageFileNameTB;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ImageFileNameTB;
+        private System.Windows.Forms.Button StartCameraButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label FpsLabel;
+        private System.Windows.Forms.Label fpsLabelLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox DebugTextBox;
+        private System.Windows.Forms.Timer OneSecondTimer;
     }
 }
 
--- EZFormApplication/Form1.cs
+++ EZFormApplication/Form1.cs
@@ -9,12 +9,19 @@
 
     public partial class Form1 : Form
     {
+        private bool isClosing;
         private EZB ezb;
-        private EventWaitHandle startEvent;
+        private EventWaitHandle frameEventObject;
         private Camera camera;
+        private long fpsCounter;
 
         public void WriteDebug(object obj, bool clear = false)
         {
+            if (this.isClosing)
+            {
+                return;
+            }
+
             string text = obj.ToString();
             this.DebugTextBox.Invoke(new EventHandler(delegate
             {
@@ -36,64 +43,155 @@
             this.ImageFileNameTB.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "capture-" + DateTime.Now.ToString("yyyyxMMxddxHHxmmxss") + ".jpg");
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ezb = new EZB();
+            this.camera = new Camera(this.ezb);
+            this.camera.OnStart += this.CameraOnOnStart;
+            this.camera.OnNewFrame += this.CameraOnOnNewFrame;
+            this.camera.OnStop += this.CameraOnStop;
+
+            //create event object to avoid sleep/poll check
+            this.frameEventObject = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+            ////create a timer to display the number of frames per second 
+            //this.fpsTimer = new System.Windows.Forms.Timer();
+            //this.fpsTimer.Interval = 60 * 1000; // 1 second
+            //this.fpsTimer.Tick += this.FpsTimer_Tick;
+            //this.fpsTimer.Enabled;
+            this.OneSecondTimer.Enabled = true;
+            this.OneSecondTimer.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.isClosing = true;
+            //clean up
+            if (this.camera != null)
+            {
+                if (this.camera.IsActive)
+                {
+                    this.camera.StopCamera();
+                }
+                this.camera.Dispose();
+            }
+            if (this.ezb != null)
+            {
+                if (this.ezb.IsConnected)
+                {
+                    this.ezb.Disconnect();
+                }
+                this.ezb.Dispose();
+            }
+        }
+
         private void TakeAPicButton_Click(object sender, EventArgs e)
         {
-            this.DebugTextBox.Text = "";
+            //to avoid concurrency issues
+            this.StartCameraButton.Enabled = false;
+            this.TakeAPicButton.Enabled = false;
 
             try
             {
-                using (ezb = new EZB())
+                //current camera state
+                //if camera is stopped => start camera => take a pic => stop camera
+                //if camera is started => take a pic
+                bool lastCameraIsActive = this.camera.IsActive;
+
+                if (!lastCameraIsActive)
                 {
+                    this.StartOrStopCamera();
+                }
 
-                    WriteDebug("Connecting to EZ-B @ " + this.IpAddressTB.Text);
-                    ezb.Connect(this.IpAddressTB.Text);
+                //wait 10 seconds for the first camera frame
+                if (this.frameEventObject.WaitOne(10000))
+                {
+                    this.camera.SaveImageAsJPEG(this.ImageFileNameTB.Text);
+                    this.WriteDebug("Picture done!");
+                }
+                else
+                {
+                    this.WriteDebug("Error: Unable to get a valid camera frame!");
+                }
 
-                    if (!ezb.IsConnected)
-                    {
-                        WriteDebug("Error: Unable to connect to EZ-B");
-                        return;
-                    }
-
-                    using (camera = new EZ_B.Camera(ezb))
-                    {
-                        startEvent = new EventWaitHandle(false, EventResetMode.AutoReset);
-
-                        camera.OnStart += CameraOnOnStart;
-                        camera.OnNewFrame += CameraOnOnNewFrame;
-                        camera.StartCamera(new ValuePair("EZB://" + this.IpAddressTB.Text), 160, 120);
-
-                        //wait 10 seconds for the first camera frame
-                        if (startEvent.WaitOne(10000))
-                        {
-                            //Save an image on the desktop
-                            camera.SaveImageAsJPEG(this.ImageFileNameTB.Text);
-                            this.pictureBox1.Image = camera.GetCurrentBitmap;
-                            camera.StopCamera();
-                        }
-                        else
-                        {
-                            WriteDebug("Error: Unable to get the picture!");
-                        }
-                    }
-
-                    WriteDebug("Done.");
-                    ezb.Disconnect();
+                if (!lastCameraIsActive)
+                {
+                    this.StartOrStopCamera();
                 }
             }
             catch (Exception ex)
             {
                 this.WriteDebug("Error Exception=" + ex);
             }
+
+            this.StartCameraButton.Enabled = true;
+            this.TakeAPicButton.Enabled = true;
+        }
+
+        private void StartCameraButton_Click(object sender, EventArgs e)
+        {
+            //to avoid concurrency issues
+            this.StartCameraButton.Enabled = false;
+            this.TakeAPicButton.Enabled = false;
+
+            try
+            {
+                this.StartOrStopCamera();
+            }
+            catch (Exception ex)
+            {
+                this.WriteDebug("Error Exception=" + ex);
+            }
+
+            this.StartCameraButton.Enabled = true;
+            this.TakeAPicButton.Enabled = true;
+        }
+
+
+        private void CameraOnOnStart()
+        {
+            this.WriteDebug("Camera Started!");
         }
 
         private void CameraOnOnNewFrame()
         {
-            startEvent.Set();
+            this.frameEventObject.Set();
+            this.pictureBox1.Invoke(new EventHandler(delegate { this.pictureBox1.Image = this.camera.GetCurrentBitmap; }));
+            Interlocked.Increment(ref this.fpsCounter);
         }
 
-        private void CameraOnOnStart()
+        private void CameraOnStop()
         {
-            WriteDebug("Camera Started!");
+            this.WriteDebug("Camera Stopped!");
+            this.frameEventObject.Reset();
+        }
+
+        private void OneSecondTimer_Tick(object sender, EventArgs e)
+        {
+            var cnt = Interlocked.Read(ref this.fpsCounter);
+            Interlocked.Exchange(ref this.fpsCounter, 0);
+            this.FpsLabel.Text = cnt.ToString(CultureInfo.InvariantCulture);
+            this.fpsLabelLabel.Visible = this.FpsLabel.Visible = cnt > 0;
+        }
+
+
+        private void StartOrStopCamera()
+        {
+            var labels = this.StartCameraButton.Tag.ToString().Split(new[] { '|' });
+
+            if (this.camera.IsActive)
+            {
+                this.WriteDebug("Stopping camera.");
+                this.camera.StopCamera();
+                this.StartCameraButton.Text = labels[0];
+            }
+            else
+            {
+                this.WriteDebug("Starting camera.");
+                this.frameEventObject.Reset();
+                this.camera.StartCamera(new ValuePair("EZB://" + this.IpAddressTB.Text), 160, 120);
+                this.StartCameraButton.Text = labels[1];
+            }
         }
     }
 }
